efc07eab3d9ed1416cbad0ae497bc52518b7157d
Josue Efrain Avelar Dominguez <joshy_je@hotmail.com>
2022-03-02 20:45:01 -0600

    Commit incial
    Primer maquetado del sitio, página principal y acerca de.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'media',
+  content: ["./src/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
